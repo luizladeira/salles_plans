8f2db4966ad9e0fbb58742a768a6c8f78f337f6a
luiz.ladeira <luizpauloladeira@hotmail.com>
2022-12-27 17:30:47 -0300

    ajustando as features

--- resources/views/site/_partials/plans.blade.php
+++ resources/views/site/_partials/plans.blade.php
@@ -38,7 +38,14 @@
                                     </svg> <span x-text="feature"></span></p>
                             </template>
 
-                        {!!$plan->description!!}
+
+                        @foreach($plan->features as $feature)
+                            <p class="feature"><svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor"
+                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M5 13l4 4L19 7"></path>
+                                </svg> <span x-text="feature">{{$feature->name}}</span></p>
+                        @endforeach
 
                         </div>
                         <div class="card__footer text-center my-4"><a href="#"
